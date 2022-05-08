ee53b7ab0b4c2b63dbb9b9099db057017386d217
1liornatan <1liornatan@gmail.com>
2022-05-08 23:06:48 +0300

    file argument check program description added

--- AlgoProject/main.cpp
+++ AlgoProject/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include "InputManager.h"
 
+/* the program receives a file path in the first argument
+   the file contains input as showed in the examples
+   if the input is invalid a message will be received
+   otherwise, an MST will be calculated (if exists)  */
+
 int main(int argc, char* argv[])
 {
-	InputManager input(argv[1]);
+	if (argc <= 1)
+		std::cout << "no file argument received" << std::endl;
+	else {
+		InputManager input(argv[1]);
 
-	input.start();
+		input.start();
+	}
 }
